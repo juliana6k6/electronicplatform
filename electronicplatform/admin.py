f18d3c2a8af7000207fdfc8e627e0581ed9eadc4
Juliana <juliana6k@mail.ru>
2024-09-13 11:27:49 +0300

    Добавили csu, админпанель для платформы

--- electronicplatform/admin.py
+++ electronicplatform/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from retail.models import NetworkUnit, Product
+
+
+@admin.action(description="Очистить задолженность перед поставщиком")
+def clear_debt(modeladmin, request, queryset):
+    queryset.update(debt=0.00)
+
+
+@admin.register(NetworkUnit)
+class PlatformUnitAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "country",
+        "city",
+        "type"
+        "level",
+        "supplier",
+        "debt",
+        "date_of_create",
+        "email"
+    )
+    list_filter = ("city",)
+    search_fields = ("name",)
+    actions = [clear_debt]
+    list_display_links = ["supplier"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "model",
+        "release_date",
+        "network_unit",
+    )
+    list_filter = ("network_unit",)
+    search_fields = ("name",)
+    list_display_links = ["network_unit"]
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Отображение торговой сети в панели администратора"""
+    list_display = ('id', 'email', 'is_active', 'is_superuser', "first_name", "last_name")
